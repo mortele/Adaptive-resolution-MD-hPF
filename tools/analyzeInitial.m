5d2a4fedca334d916b7f2db3fd2c34791edde3ca
Morten Ledum <mortele@student.matnat.uio.no>
2018-08-22 15:59:13 +0200

    Simple visualization matlab plot added

---
+++ tools/analyzeInitial.m
@@ -0,0 +1,21 @@
+%% Clean up
+close all;
+clear variables;
+clc;
+format;
+
+
+%% Load .xyz file
+fileID  = fopen('../positions.xyz', 'r');
+data    = textscan(fileID, '%d %f %f %f', 'HeaderLines', 2);
+
+% Organize cell array into vectors.
+type = data{1};
+x    = data{2};
+y    = data{3};
+z    = data{4};
+
+
+%% Plot the particle positions
+hFig = figure();
+hPlot = scatter3(x, y, z, type .* 500, 'k.');
